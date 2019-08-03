e191b47c4a2d6c32978481d6b356ee970c356b4b
M.V.S. SRIHARSHA <sriharshamvs@gmail.com>
2019-08-03 14:25:36 +0530

    Upload New File

---
+++ chatterbot/Chatbot.py
@@ -0,0 +1,20 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('GRIET')
+
+trainer = ListTrainer(bot)
+
+for files in os.listdir("/home/mvs/Workspace/Python/Chatbots_tutorials/chatbot_corpus/chatterbot_corpus/data/english"):
+    data = open("/home/mvs/Workspace/Python/Chatbots_tutorials/chatbot_corpus/chatterbot_corpus/data/english/"+files, 'r').readlines()
+    trainer.train(data)
+
+while True:
+    try:
+        message = str(input('You: '))
+        bot_input = bot.get_response(message)
+        print('GRIET:',bot_input)
+
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
